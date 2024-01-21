ef01c5b0969409cfdc457ad0485fa220a40bf217
Tanmay <tanmaychoudhary4102@gmail.com>
2024-01-22 01:25:08 +0530

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,18 @@ import { Provider } from "react-redux";
 import store, { persistor } from "../src/redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import "../styles/globals.css";
+import Head from "next/head";
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Component {...pageProps} />
-      </PersistGate>
-    </Provider>
+    <>
+      <Head>
+        <title>Tanmay Assignment</title>
+      </Head>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
+    </>
   );
 }
